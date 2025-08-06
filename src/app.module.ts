2100da948dfc3824f82852e85faf7518c4803150
ialnezami <ibrahim.alnezami@gmail.com>
2025-08-06 06:44:19 +0300

    Add Users, Appointments, and Timeslots modules with controllers, services, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { TimeslotsModule } from './timeslots/timeslots.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule, AppointmentsModule, TimeslotsModule],
   controllers: [AppController],
   providers: [AppService],
 })
